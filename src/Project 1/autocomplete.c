f404ea49c0c004b77b2cd91728fda5b75c3839c8
Eric9867 <32530980+Eric9867@users.noreply.github.com>
2021-03-02 22:17:11 -0500

    Project 1: sorting and fixing no-match case * implement sorting directly in the read_in_terms function * fix using scanf_s (replace with fgets()) and apply sscanf after * fix program crash when submitting a request with no matches
    * Full credit to Derek Ma for the validation and testing files

--- src/Project 1/autocomplete.c
+++ src/Project 1/autocomplete.c
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -5,80 +6,113 @@
 
 #define TERM_SIZE 200
 
-void read_in_terms(struct term **terms, int *pnterms, char *filename)
+int term_comparison(const void* term_1, const void* term_2)
 {
-    FILE* handle;
-    if(fopen_s(&handle, filename, "r"))
-        abort(); /* Better error handling is recommended */
-    
-    /* Read number of lines (first line of file) */
-    fscanf_s( handle, " %d ", pnterms, sizeof(int) );
-    
-    *terms = (struct term*)malloc(*pnterms * sizeof(struct term)); 
-    /* loop through the file, creating one term per line */
-    /** Examples
-     *    14608512	Shanghai, China
-     *  5627187200	the
-     */
-    for(
-        int i = 0;    
-        fscanf_s(
-            handle, 
-            "%lf %[^\n] ", 
-            &(*terms)[i].weight,
-            (*terms)[i].term, TERM_SIZE
-        ) > 0;
-        i++
-    ); // {printf("%s\n", (*terms)[i].term);}
+  return strcmp(((struct term*)term_1)->term, ((struct term*)term_2)->term);
+}
 
-    fclose(handle);
+void read_in_terms(struct term** terms, int* pnterms, char* filename)
+{
+  FILE* handle = fopen(filename, "r");
+  if (!handle)
+    abort(); /* Better error handling is recommended */
+
+/* Read number of lines (first line of file) */
+  char line[200];
+  if (fgets(line, sizeof(line), handle) <= 0)
+    abort(); /* check for EOF (empty file) ??? */
+  *pnterms = atoi(line);
+  *terms = (struct term*)malloc((*pnterms) * sizeof(struct term));
+  if (!*terms) 
+    abort();
+  /* loop through the file, creating one term per line */
+  /** Examples
+   *    14608512	Shanghai, China
+   *  5627187200	the
+   */
+  for (int i = 0; i < *pnterms; i++)
+  {
+    /*
+    if (
+      fscanf(
+        handle,
+        " %lf %[^\n] ",
+        &(*terms)[i].weight,
+        &(*terms)[i].term
+    ) <= 0)
+      break;
+    // {printf("%s\n", (*terms)[i].term);}
+    */
+
+    if(fgets(line, sizeof(line), handle) == NULL)
+      abort();                 //read in at most sizeof(line) characters
+                               //(including '\0') into line.
+    if (
+      sscanf(line, " %lf %199[^\n] ",
+        &(*terms)[i].weight,
+        &(*terms)[i].term
+      ) <= 0)
+    {
+      if (*pnterms > i + 1000) /* MEMORY THRESHOLD FOR REALLOCATING */
+      {
+        struct term* temp = (struct term*)realloc(*terms, sizeof(struct term) * (i - 1));
+        if (!!temp)
+          *terms = temp;
+      }
+      *pnterms = i - 1;
+      break;
+    }
+  }
+  fclose(handle);
+
+  qsort(*terms, *pnterms, sizeof(struct term), term_comparison);
 };
 
-int term_weight_comparison(const void *term_1, const void *term_2)
+int term_weight_comparison(const void* term_1, const void* term_2)
 {
-    double arg1 = ((struct term*)term_1)->weight;
-    double arg2 = ((struct term*)term_2)->weight;
-    if (arg1 > arg2) return -1;
-    else if (arg1 < arg2) return 1;
-    else return 0;
+  double arg1 = ((struct term*)term_1)->weight;
+  double arg2 = ((struct term*)term_2)->weight;
+  if (arg1 > arg2) return -1;
+  else if (arg1 < arg2) return 1;
+  else return 0;
 }
 
 
 /**
- * Looking for the lower boundary (in terms of alphabetical 
+ * Looking for the lower boundary (in terms of alphabetical
  * ordering) on strings that begin with the substring
  */
-int lowest_match(struct term *terms, int n_terms, char *substr)
+int lowest_match(struct term* terms, int n_terms, char* substr)
 {
-    int idx_lower = 0;
-    int idx_upper = n_terms;
-    int mid = idx_upper / 2;
-    size_t substr_len = strlen(substr);
-    int comparison = strncmp(substr, terms[mid].term, substr_len);
+  int idx_lower = 0;
+  int idx_upper = n_terms;
+  int mid = idx_upper / 2;
+  size_t substr_len = strlen(substr);
+  int comparison = strncmp(substr, terms[mid].term, substr_len);
 
-    while(idx_upper - idx_lower > 1){
-        if(comparison <= 0) // substr is in first half
-            idx_upper = mid;
-        else
-            idx_lower = mid;
-            
-        mid = (idx_lower + idx_upper) / 2;
-        comparison = strncmp(substr, terms[mid].term, substr_len);
-    }
-    
-    comparison = strncmp(substr, terms[idx_lower].term, substr_len);
-    if(comparison == 0)
-        return idx_lower;
-
-    comparison = strncmp(substr, terms[idx_upper].term, substr_len);
-    if(comparison == 0)
-        return idx_upper;
-    
+  while (idx_upper - idx_lower > 1) {
+    if (comparison <= 0) // substr is in first half
+      idx_upper = mid;
     else
-        return -1;
-    /**
-     * TODO: What is a suitable return value if no match was found??
-     */ 
+      idx_lower = mid;
+
+    mid = (idx_lower + idx_upper) / 2;
+    comparison = strncmp(substr, terms[mid].term, substr_len);
+  }
+
+  comparison = strncmp(substr, terms[idx_lower].term, substr_len);
+  if (comparison == 0)
+    return idx_lower;
+
+  comparison = strncmp(substr, terms[idx_upper].term, substr_len);
+  if (comparison == 0)
+    return idx_upper;
+
+  else
+    return -1;
+  /**
+   * TODO: What is a suitable return value if no match was found??
+   */
 };
 
 /**
@@ -93,58 +127,68 @@ This function must run in O(log(nterms)) time, where nterms is the number of ter
 
 You can assume that terms is sorted in ascending lexicographic ordering.
 */
-int highest_match(struct term *terms, int n_terms, char *substr)
+int highest_match(struct term* terms, int n_terms, char* substr)
 {
-    int idx_lower = 0;
-    int idx_upper = n_terms;
-    int mid = idx_upper / 2;
-    size_t substr_len = strlen(substr);
-    int comparison = strncmp(substr, terms[mid].term, substr_len);
+  int idx_lower = 0;
+  int idx_upper = n_terms;
+  int mid = idx_upper / 2;
+  size_t substr_len = strlen(substr);
+  int comparison = strncmp(substr, terms[mid].term, substr_len);
 
-    while(idx_upper - idx_lower > 1){
-        if(comparison < 0) // substr is in first half
-            idx_upper = mid;
-        else 
-            idx_lower = mid;
-            
-        mid = (idx_lower + idx_upper) / 2;
-        comparison = strncmp(substr, terms[mid].term, substr_len);
-    }
-
-    comparison = strncmp(substr, terms[idx_upper].term, substr_len);
-    if(comparison == 0)
-        return idx_upper;
-    
-    comparison = strncmp(substr, terms[idx_lower].term, substr_len);
-    if(comparison == 0)
-        return idx_lower;
-    
+  while (idx_upper - idx_lower > 1) {
+    if (comparison < 0) // substr is in first half
+      idx_upper = mid;
     else
-        return -1;
+      idx_lower = mid;
+
+    mid = (idx_lower + idx_upper) / 2;
+    comparison = strncmp(substr, terms[mid].term, substr_len);
+  }
+
+  comparison = strncmp(substr, terms[idx_upper].term, substr_len);
+  if (comparison == 0)
+    return idx_upper;
+
+  comparison = strncmp(substr, terms[idx_lower].term, substr_len);
+  if (comparison == 0)
+    return idx_lower;
+
+  else
+    return -1;
 }
 
 /**
 Part 3
 ======
-Write a function with the signature 
+Write a function with the signature
 
 void autocomplete(struct term **answer, int *n_answer, struct term *terms, int nterms, char *substr);
 
-The function takes terms (assume it is sorted), the number of terms nterms, and the query string substr, and places the 
+The function takes terms (assume it is sorted), the number of terms nterms, and the query string substr, and places the
 answers in answer, with *n_answer being the number of answers.
 */
 
-void autocomplete(struct term **answer, int *n_answer, struct term *terms, int n_terms, char *substr)
+void autocomplete(struct term** answer, int* n_answer, struct term* terms, int n_terms, char* substr)
 {
-    int idx_lower = lowest_match(terms, n_terms, substr);
+  int idx_lower = lowest_match(terms, n_terms, substr);
+  if (idx_lower == -1) /* checking either index should suffice */
+  {
+    *n_answer = 0;
+    answer = NULL;
+  }
+  else
+  {
     int idx_upper = highest_match(terms, n_terms, substr);
-    *n_answer = idx_upper - idx_lower + 1;    
-    *answer = (struct term*) malloc((*n_answer) * sizeof(struct term));
-    
-    for(int i = 0; i < *n_answer; i++)
+    *n_answer = idx_upper - idx_lower + 1;
+    *answer = (struct term*)malloc((*n_answer) * sizeof(struct term));
+    if (!*answer)
+      abort();
+
+    for (int i = 0; i < *n_answer; i++)
     {
-        (*answer)[i] = terms[idx_lower + i]; 
+      (*answer)[i] = terms[idx_lower + i];
     }
 
     qsort(*answer, *n_answer, sizeof(struct term), term_weight_comparison);
+  }
 }
--- src/Project 1/main.c
+++ src/Project 1/main.c
@@ -3,38 +3,45 @@
 #include <string.h>
 #include "autocomplete.h"
 
-int term_comparison(const void* term_1, const void* term_2)
-{
-    return strcmp(((struct term*)term_1)->term, ((struct term*)term_2)->term);
-}
-
 int main(void)
 {
-    struct term *terms;
-    int n_terms;
-    read_in_terms(&terms, &n_terms, "../src/Project 1/cities.txt");
+  struct term* terms;
+  int n_terms;
+  // read_in_terms(&terms, &n_terms, "../src/Project 1/cities.txt");
+  read_in_terms(&terms, &n_terms, "C:\\Users\\Eric\\OneDrive - University of Toronto\\Classes\\Year 1 Winter\\ESC190\\Assignments\\Labs\\src\\Project 1\\cities.txt");
 
-    // lowest_match(terms, nterms, "Tor");
-    // highest_match(terms, nterms, "Tor");
-    
-    // struct term *answer;
-    // int n_answer;
-    // autocomplete(&answer, &n_answer, terms, int nterms, "Tor");
+  // lowest_match(terms, nterms, "Tor");
+  // highest_match(terms, nterms, "Tor");
 
-    //free allocated blocks here -- not required for the project, but good practice
-    qsort(terms, n_terms, sizeof(struct term), term_comparison);
-    
-    struct term *answers;
-    int n_answers;
+  // struct term *answer;
+  // int n_answer;
+  // autocomplete(&answer, &n_answer, terms, int nterms, "Tor");
 
-    autocomplete(&answers, &n_answers, terms, n_terms, "Tor");
+  //free allocated blocks here -- not required for the project, but good practice
+
+  struct term* answers;
+  int n_answers;
+
+  autocomplete(&answers, &n_answers, terms, n_terms, "Ver");
+
+  // printf("# TERMS: %d,  # ANSWERS: %d\n", n_terms, n_answers);
+
+  for (int i = 0; i < n_answers; i++)
+    printf("%lf,\t%s\n", answers[i].weight, answers[i].term);
 
 
-    for( int i = 0; i < n_answers; i++ )
-        printf("%lf, %s\n", answers[i].weight, answers[i].term);
+  // printf("\nSTART\n");
+  // printf("%s\n", answers[0].term);
+  // for (int i = 0; answers[0].term[i]; i++)
+  //   printf("%c\n", answers[0].term[i]);
 
-    free(answers);
+
+  if (n_terms != 0)
+  {
     free(terms);
-    return 0;
+    if (n_answers != 0)
+      free(answers);
+  }
+  return 0;
 }
 
